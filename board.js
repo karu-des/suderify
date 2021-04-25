db534763393a780b742cc73c4fe6957d1a0e1c29
karu-des <karuchan76@email.com>
2021-04-25 13:54:29 -0700

    old board removal 😔

--- board.js
+++ board.js
@@ -88,10 +88,7 @@ class Board {
     }
 
     removeOldBoard() {
-        let board = document.getElementById("board");
-        while (board.firstChild) {
-            board.removeChild(board.firstChild);
-        }
+        document.getElementById("board").innerHTML = "";
     }
 }
 
